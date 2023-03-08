f6f128f3bf67238d98a029860b3ecc0b0c6f3300
TROCHIM-PC\Trochim <mateusztrochimowicz@gmail.com>
2023-03-08 20:49:04 +0100

    loading

---
+++ Loading/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+  // Upewniamy się, że element o klasie ".loading" istnieje
+  const loader = document.querySelector(".loading");
+  if (!loader) {
+    return;
+  }
+});
