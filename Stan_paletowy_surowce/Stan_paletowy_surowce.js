607ab4f6689c4d05b9a42a085221ca386d4a9765
TROCHIM-PC\Trochim <mateusztrochimowicz@gmail.com>
2023-03-13 21:20:18 +0100

    alert wygląd i css

--- Stan_paletowy_surowce/Stan_paletowy_surowce.js
+++ Stan_paletowy_surowce/Stan_paletowy_surowce.js
@@ -3,6 +3,8 @@ let rawmaterial = document.getElementById("rawmaterial");
 let andersa = document.getElementById("andersa");
 let finishedProduct = document.getElementById("finished_product");
 let count = document.getElementById("btn1");
+let yesterdayElement = document.querySelector(".yesterday");
+yesterdayElement.classList.add("delivery_yesterday");
 
 function countresult() {
   let freeValue = parseInt(free.value);
@@ -10,14 +12,31 @@ function countresult() {
   let andersaValue = parseInt(andersa.value);
   let finishedProductValue = parseInt(finishedProduct.value);
 
+  if (
+    isNaN(freeValue) ||
+    isNaN(rawmaterialValue) ||
+    isNaN(andersaValue) ||
+    isNaN(finishedProductValue)
+  ) {
+    $(".alert").addClass("show");
+    $(".alert").removeClass("hide");
+    $(".alert").addClass("showAlert");
+
+    $(".close-btn").click(function () {
+      $(".alert").removeClass("show");
+      $(".alert").addClass("hide");
+    });
+
+    setTimeout(function () {
+      $(".alert").removeClass("show");
+      $(".alert").addClass("hide");
+    }, 5000000);
+
+    return;
+  }
+
   let countresult =
     2040 - freeValue - finishedProductValue + rawmaterialValue + andersaValue;
-  let historyElement = document.getElementById("history");
-  let timestamp = new Date().toLocaleDateString();
-  let calculation = `${timestamp}: ${freeValue} + ${rawmaterialValue} - ${finishedProductValue} + ${andersaValue} = ${countresult}`;
-  let calculationElement = document.createElement("p");
-  calculationElement.textContent = calculation;
-  historyElement.appendChild(calculationElement);
 
   document.getElementById("result").innerHTML = countresult;
 
@@ -61,4 +80,34 @@ function sendEmail() {
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+}
+
+// Znajdź przycisk "Oblicz" na stronie i dodaj obsługę zdarzenia kliknięcia
+let calculateButton = document.getElementById("btn2");
+calculateButton.addEventListener("click", resultspentpallets);
+
+function resultspentpallets() {
+  // Pobierz wartości z pól wejściowych
+  let yesterdayValue = parseInt(document.getElementById("yesterday").value);
+  let deliveryYesterdayValue = parseInt(
+    document.getElementById("delivery_yesterday").value
+  );
+
+  // Oblicz wynik
+  let result_spentPallets =
+    yesterdayValue + deliveryYesterdayValue - countresult();
+
+  // Ustaw wynik w elemencie "result_spent"
+  let resultElement = document.getElementById("result_spent");
+  resultElement.innerHTML = result_spentPallets;
+}
+
+const inputElements = document.querySelectorAll("input[type=number]");
+
+for (let i = 0; i < inputElements.length; i++) {
+  inputElements[i].addEventListener("keydown", function (event) {
+    if (event.key === "-") {
+      event.preventDefault();
+    }
+  });
 }
